b3fb86857b993e8a8fd25fed3a2afde136391a35
Favas Muhammed <favassakkeer13@gmail.com>
2024-10-03 12:58:31 +0200

    changes done

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-require('dotenv').config()
-const jsonServer = require('json-server')
-const morgan = require('morgan')
+require("dotenv").config();
+const jsonServer = require("json-server");
+const morgan = require("morgan");
 
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-const dbData = require('./db.json')
+const db = require("./db.json");
+const router = jsonServer.router(db);
 
-const router = jsonServer.router(dbData)
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT;
 
-const middlewares = jsonServer.defaults()
-const PORT = process.env.PORT
-
-server.use(middlewares)
-server.use(morgan('dev'))
+server.use(middlewares);
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  // Middleware to disable CORS
-  res.header('Access-Control-Allow-Origin', '*')
-  next()
-})
-server.use(router)
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
